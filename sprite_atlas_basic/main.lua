ce04f9f7fc5c0cacea87ad3080dade04d4da348e
Leon Kasovan <leonkasovan@gmail.com>
2025-03-10 04:25:15 +0700

    change lint format

--- sprite_atlas_basic/main.lua
+++ sprite_atlas_basic/main.lua
@@ -8,8 +8,8 @@ local tick = 0
 local action_id = 0
 
 zangief_actions = {
-    [1] = function() return 0 end,
-    [2] = function() return 1234 end,
+	[1] = function() return 0 end,
+	[2] = function() return 1234 end,
 	[3] = function() return 688 end,
 	[4] = function() return 788 end,
 	[5] = function() return 888 end,
@@ -22,12 +22,12 @@ zangief_actions = {
 	[12] = function() return 41 end,
 	[13] = function() return 96 end,
 	[14] = function() return 20 end,
-    [15] = function() return 21 end
+	[15] = function() return 21 end
 }
 
 ken_actions = {
-    [1] = function() return 0 end,
-    [2] = function() return 5 end,
+	[1] = function() return 0 end,
+	[2] = function() return 5 end,
 	[3] = function() return 20 end,
 	[4] = function() return 21 end,
 	[5] = function() return 22 end,
@@ -40,12 +40,12 @@ ken_actions = {
 	[12] = function() return 181 end,
 	[13] = function() return 192 end,
 	[14] = function() return 195 end,
-    [15] = function() return 210 end
+	[15] = function() return 210 end
 }
 
 ryu_actions = {
-    [1] = function() return 0 end,
-    [2] = function() return 5 end,
+	[1] = function() return 0 end,
+	[2] = function() return 5 end,
 	[3] = function() return 20 end,
 	[4] = function() return 21 end,
 	[5] = function() return 22 end,
@@ -58,29 +58,30 @@ ryu_actions = {
 	[12] = function() return 181 end,
 	[13] = function() return 192 end,
 	[14] = function() return 195 end,
-    [15] = function() return 210 end
+	[15] = function() return 210 end
 }
 
 function trim(s)
-    return s:match("^%s*(.-)%s*$")
+	return s:match("^%s*(.-)%s*$")
 end
 
 function split(inputstr)
-    local t = {}
-    for str in string.gmatch(inputstr, "([^,]+)") do
-        table.insert(t, trim(str))
-    end
-    return t
+	local t = {}
+	for str in string.gmatch(inputstr, "([^,]+)") do
+		table.insert(t, trim(str))
+	end
+	return t
 end
 
 function loadChar(name, x, y)
 	player = {}
 	player.name = name
-	player.atlas_img = love.graphics.newImage(player.name..".png")
+	player.atlas_img = love.graphics.newImage(player.name .. ".png")
 	player.atlas_dat = {}
-	for line in io.lines(player.name..".tsv") do -- Iterate through each line of player.tsv (tab separated values)
+	for line in io.lines(player.name .. ".tsv") do -- Iterate through each line of player.tsv (tab separated values)
 		if #line > 0 then
-			src_x, src_y, src_w, src_h, dst_x, dst_y, dst_w, dst_h, spr_group_id, spr_img_no= line:match("(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)%D(%d+)")
+			src_x, src_y, src_w, src_h, dst_x, dst_y, dst_w, dst_h, spr_group_id, spr_img_no = line:match(
+				"(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)\t(%d+)%D(%d+)")
 			src_x = tonumber(src_x)
 			src_y = tonumber(src_y)
 			src_w = tonumber(src_w)
@@ -91,12 +92,12 @@ function loadChar(name, x, y)
 			dst_h = tonumber(dst_h)
 			spr_group_id = tonumber(spr_group_id)
 			spr_img_no = tonumber(spr_img_no)
-			
+
 			-- Ensure atlas_dat[group_id] is a table before assigning values
 			if player.atlas_dat[spr_group_id] == nil then
-				player.atlas_dat[spr_group_id] = {}  -- Create a new table for this key
+				player.atlas_dat[spr_group_id] = {} -- Create a new table for this key
 			end
-			player.atlas_dat[spr_group_id][spr_img_no] = {src_x, src_y, src_w, src_h, dst_x, dst_y, dst_w, dst_h}
+			player.atlas_dat[spr_group_id][spr_img_no] = { src_x, src_y, src_w, src_h, dst_x, dst_y, dst_w, dst_h }
 		end
 	end
 	player.atlas_img_w, player.atlas_img_h = player.atlas_img:getDimensions()
@@ -106,28 +107,27 @@ function loadChar(name, x, y)
 	player.y = y
 	player.tick = 0
 	player.frame_no = 1
-	
+
 	local action_id = nil
 	local new_action_id = nil
 	player.anims = {}
-	for line in io.lines(player.name..".air") do
+	for line in io.lines(player.name .. ".air") do
 		line = trim(line)
-		if #line == 0 then goto skip end	-- check if empty line
-		if string.byte(line, 1) == 59 then goto skip end	-- check if commented using ';' = byte 59
-		
+		if #line == 0 then goto skip end           -- check if empty line
+		if string.byte(line, 1) == 59 then goto skip end -- check if commented using ';' = byte 59
+
 		-- parse [Begin Action ID]
 		new_action_id = line:match("^%[[Bb][Ee][Gg][Ii][Nn]%s+[Aa][Cc][Tt][Ii][Oo][Nn]%s+(%d+)%]$")
-		
+
 		if new_action_id ~= nil then
---~ 			print("Process action", new_action_id, action_id)
 			action_id = tonumber(new_action_id)
 			goto skip
 		end
-		
+
 		-- parse animation element: spr_group_id, spr_img_no, spr_x, spr_y, ticks, flips, color blending
-		spr_group_id, spr_img_no, spr_x, spr_y, last_data = line:match("(%d+)%s*,%s*(%d+)%s*,%s*([%d%-]+)%s*,%s*([%d%-]+)%s*,%s*(.-)$")
+		spr_group_id, spr_img_no, spr_x, spr_y, last_data = line:match(
+			"(%d+)%s*,%s*(%d+)%s*,%s*([%d%-]+)%s*,%s*([%d%-]+)%s*,%s*(.-)$")
 		if action_id ~= nil and spr_group_id ~= nil and last_data ~= nil then
---~ 			print("\t", spr_group_id, spr_img_no, spr_x, spr_y, last_data)
 			spr_group_id = tonumber(spr_group_id)
 			spr_img_no = tonumber(spr_img_no)
 			spr_x = tonumber(spr_x)
@@ -149,7 +149,7 @@ function loadChar(name, x, y)
 				else
 					if anim.ticks < 0 then
 						anim.ticks = 20
-					end	
+					end
 				end
 			end
 			anim.flip = nil
@@ -157,36 +157,35 @@ function loadChar(name, x, y)
 			table.insert(player.anims[action_id], anim)
 			goto skip
 		end
-		
+
 		::skip::
 	end
-	
+
 	return player
 end
 
 function love.load()
 	love.window.setVSync(1)
-	
-	table.insert(players, loadChar("Zangief", 0, 20))	
+
+	table.insert(players, loadChar("Zangief", 0, 20))
 	table.insert(players, loadChar("Zangief", 200, 20))
 	table.insert(players, loadChar("Zangief", 400, 20))
 	table.insert(players, loadChar("Zangief", 600, 20))
-	
+
 	table.insert(players, loadChar("Zangief", 0, 200))
 	table.insert(players, loadChar("Zangief", 200, 200))
 	table.insert(players, loadChar("Zangief", 400, 200))
 	table.insert(players, loadChar("Zangief", 600, 200))
-	
+
 	table.insert(players, loadChar("Zangief", 0, 400))
 	table.insert(players, loadChar("Zangief", 200, 400))
 	table.insert(players, loadChar("Zangief", 400, 400))
 	table.insert(players, loadChar("Zangief", 600, 400))
 end
 
-
 function love.update(dt)
 	local dt, anim
-	
+
 	for k, player in pairs(players) do
 		player.sprites:clear()
 		player.tick = player.tick + 1
@@ -212,9 +211,11 @@ function love.update(dt)
 					else
 						dt = player.atlas_dat[anim.spr_group_id][anim.spr_img_no]
 					end
-					
+
 					if dt ~= nil then
-						player.sprites:add(love.graphics.newQuad(dt[1], dt[2], dt[3], dt[4], player.atlas_img_w, player.atlas_img_h), player.x + dt[5] + anim.spr_x, player.y + dt[6] + anim.spr_y)
+						player.sprites:add(
+							love.graphics.newQuad(dt[1], dt[2], dt[3], dt[4], player.atlas_img_w, player.atlas_img_h),
+							player.x + dt[5] + anim.spr_x, player.y + dt[6] + anim.spr_y)
 					else
 						print(string.format("atlas_dat is nil, group=%d img_no=%d", anim.spr_group_id, anim.spr_img_no))
 					end
@@ -222,8 +223,6 @@ function love.update(dt)
 					print(string.format("anim is nil, state=%d frame=%d", player.state, player.frame_no))
 				end
 			end
-			
-			
 		end
 	end
 end
@@ -232,7 +231,7 @@ function love.draw()
 	-- Finally, draw the sprite batch to the screen.
 	for k, player in pairs(players) do
 		love.graphics.draw(player.sprites)
-		love.graphics.print("Action: "..tostring(player.state), player.x, player.y+150)
+		love.graphics.print("Action: " .. tostring(player.state), player.x, player.y + 150)
 	end
-	love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 700, 0)
+	love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS()), 700, 0)
 end
